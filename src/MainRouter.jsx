91cc56685237248c6ef745e5666262c22e0e0627
Hoooao <hao021014@163.com>
2022-11-08 20:19:58 -0500

    mile2

---
+++ src/MainRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignInSide from './containers/SignInSide'
+import Header from './containers/Header'
+
+const MainRouter = () => {
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<SignInSide />} />
+            </Routes>
+
+
+        </div>
+    )
+}
+
+export default MainRouter;
